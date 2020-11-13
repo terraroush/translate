e203a3754a7a31fc20ab7f819b79a6f128d08d6f
Terra Roush <terra.roush@gmail.com>
2020-11-13 14:17:43 -0600

    translated csharp, mapped place names, then mapped and filtered to display only places that start with the

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,15 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+  console.log("All Place Names")
+
+  names.map(name => console.log(name))
+
+  console.log("")
+
+  const theNames = names.filter(n => n.startsWith("The"))
+  theNames.map(name => console.log(name))
 
 }
 
