0bd00401b08ac0b020dca8ab04b82b936eee4681
Terra Roush <terra.roush@gmail.com>
2020-11-13 15:20:48 -0600

    translated csharp to javascript for enemies list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,49 @@
 function main() {
   // Put your code here
+  console.log("My Enemies List!")
+  console.log("----------------")
+
+  const enemies = [
+    {
+      firstName: "Joshua",
+      lastName: "Flowers",
+      offenses: ["Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"],
+      isReallyHated: true
+    },
+    {
+      firstName: "Darth",
+      lastName: "Vadar",
+      offenses: ["Cut off Luke's hand",
+        "Murdered all those kids",
+        "Unkind management practices"],
+      isReallyHated: false
+    },
+    {
+      firstName: "Betty",
+      lastName: "Rudelady",
+      offenses: ["Phone calls in the theater",
+        "Phone calls on the bus",
+        "Phone calls in line at the grocery store",
+        "Poor conversationalist"],
+      isReallyHated: true
+    },
+    {
+      firstName: "Leon",
+      lastName: "Peck",
+      offenses: ["Keeps giving me a hotplate"],
+      isReallyHated: false
+    },
+  ]
+
+  enemies.map(enemy => {
+    if (enemy.isReallyHated) {
+      console.log(`${enemy.firstName} ${enemy.lastName} Really, really dislike!`)
+    } else {
+      console.log(`${enemy.firstName} ${enemy.lastName}`)
+    }
+  })
+
 
 }
 
