352e3b8830fb62d02ec11cf295326056c41183eb
Terra Roush <terra.roush@gmail.com>
2020-11-18 10:58:24 -0600

    dice game

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,5 +1,29 @@
 function main() {
   // Put your code here
+  console.log("Let's roll some dice, baby!");
+  console.log("---------------------------");
+
+  const roll = () => {
+    const randomNum = Math.floor(Math.random() * 3) + 1
+
+    return {
+      value: randomNum
+    }
+  }
+
+
+  for (let i = 0; i < 10; i++) {
+
+    const die1 = roll()
+    const die2 = roll()
+    let message = `? + ? = ${die1.value + die2.value}`
+
+    console.log(message)
+
+    if (die1.value === die2.value) {
+      message += " DOUBLES!"
+    }
+  }
 
 }
 
