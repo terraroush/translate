5c3b1b9fb209fbad87ae624d61770792507a7f86
Terra Roush <terra.roush@gmail.com>
2020-11-18 13:41:09 -0600

    array of spells is happening

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,6 +1,35 @@
 function main() {
   // Put your code here
+  console.log("Do you believe in magic?");
+  console.log("------------------------");
 
+  allSpells = [
+    {
+      Name = "Turn enemy into a newt",
+      IsEvil = true,
+      EnergyReqired = 5.1
+    },
+    {
+      Name = "Conjure some gold for a local charity",
+      IsEvil = false,
+      EnergyReqired = 2.99
+    },
+    {
+      Name = "Give a deaf person the ability to heal",
+      IsEvil = false,
+      EnergyReqired = 12.2
+    },
+    {
+      Name = "Make yourself emperor of the universe",
+      IsEvil = true,
+      EnergyReqired = 100.0
+    },
+    {
+      Name = "Convince your relatives your political views are correct",
+      IsEvil = false,
+      EnergyReqired = 2921.5
+    }
+  ]
 }
 
 main();
