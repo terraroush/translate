8dc06afd72a00173ae1a4f22ce4586d26d2ceb04
Terra Roush <terra.roush@gmail.com>
2020-11-18 13:46:21 -0600

    syntax error fixed

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -5,29 +5,29 @@ function main() {
 
   allSpells = [
     {
-      Name = "Turn enemy into a newt",
-      IsEvil = true,
-      EnergyReqired = 5.1
+      Name: "Turn enemy into a newt",
+      IsEvil: true,
+      EnergyReqired: 5.1
     },
     {
-      Name = "Conjure some gold for a local charity",
-      IsEvil = false,
-      EnergyReqired = 2.99
+      Name: "Conjure some gold for a local charity",
+      IsEvil: false,
+      EnergyReqired: 2.99
     },
     {
-      Name = "Give a deaf person the ability to heal",
-      IsEvil = false,
-      EnergyReqired = 12.2
+      Name: "Give a deaf person the ability to heal",
+      IsEvil: false,
+      EnergyReqired: 12.2
     },
     {
-      Name = "Make yourself emperor of the universe",
-      IsEvil = true,
-      EnergyReqired = 100.0
+      Name: "Make yourself emperor of the universe",
+      IsEvil: true,
+      EnergyReqired: 100.0
     },
     {
-      Name = "Convince your relatives your political views are correct",
-      IsEvil = false,
-      EnergyReqired = 2921.5
+      Name: "Convince your relatives your political views are correct",
+      IsEvil: false,
+      EnergyReqired: 2921.5
     }
   ]
 }
